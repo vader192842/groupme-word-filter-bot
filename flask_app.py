da76d92788e0258557d3c37d90ccf1378e95fab8
vader192842 <raushan.athwal.10@gmail.com>
2025-08-10 15:07:34 -0400

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -42,6 +42,6 @@ def receive_message():
     return "OK", 200
 
 if __name__ == '__main__':
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
